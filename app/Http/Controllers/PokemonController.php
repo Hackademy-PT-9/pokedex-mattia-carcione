a2ad1460c423f67e7449bf928e5ae933340a53af
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-09-12 19:27:13 +0200

    aggiornata fetch dell'pokeApi

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -12,9 +12,10 @@ class PokemonController extends Controller
     public function fetchAndStorePokemonData()
     {
         $client = new Client();
+        $limit = 1008; //imposta il limite di dati da implementare nel database
 
         // Esegui la richiesta per ottenere la lista dei Pokémon
-        $response = $client->get("https://pokeapi.co/api/v2/pokemon?limit=1008&offset=0");
+        $response = $client->get("https://pokeapi.co/api/v2/pokemon?limit={$limit}&offset=0");
         $pokemonList = json_decode($response->getBody(), true);
 
         foreach ($pokemonList['results'] as $pokemon) {
@@ -26,6 +27,34 @@ class PokemonController extends Controller
             $pokedexResponse = $client->get("https://pokeapi.co/api/v2/pokemon-species/{$pokemonData['id']}");
             $pokedexData = json_decode($pokedexResponse->getBody(), true);
             $pokedexDescription = $pokedexData['flavor_text_entries'][0]['flavor_text'];
+
+            // Archivia i dati nel database
+            Pokemon::create([
+                'name' => $pokemonData['name'],
+                'pokedex_number' => $pokemonData['id'],
+                'pokedex_description' => $pokedexDescription,
+                'stats' => json_encode($pokemonData['stats']),
+                'type' => $pokemonData['types'][0]['type']['name'],
+                'image_url' => $pokemonData['sprites']['front_default'],
+            ]);
+        }
+
+        return 'Dati Pokémon archiviati nel database con successo.';
+
+    }
+
+    public function getAllPokemon()
+    {
+
+        $pokemonList = Http::get("https://pokeapi.co/api/v2/pokemon?limit={$limit}&offset=0")->json();
+
+        foreach ($pokemonList['results'] as $pokemon) {
+            // Esegui una richiesta per ottenere i dati del singolo Pokémon
+            $pokemonData = Http::get($pokemon['url'])->json();
+
+            // Esegui una richiesta per ottenere la descrizione del Pokédex
+            $pokedexDataResponse = Http::get("https://pokeapi.co/api/v2/pokemon-species/{$pokemonData['id']}")->json();
+            $pokedexDescription = $pokedexDataResponse['flavor_text_entries'][0]['flavor_text'];
 
             // Archivia i dati nel database
             Pokemon::create([
